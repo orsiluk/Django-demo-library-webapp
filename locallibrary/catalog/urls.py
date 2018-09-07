fb42f886b4c6e7d288d66ec48f5afcfddc1ea3c9
Orsi Lukacs <ol14474@my.bristol.ac.uk>
2018-09-07 12:09:02 +0300

    Views all connected up

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -3,5 +3,13 @@ from catalog import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), # this is implemented as function
+    path('books', views.BookListView.as_view(), name='books'), # this is implemented as class
+    # as_view() does all the work of creating an instance of the class, and making sure that the right handler methods
+    # are called for incoming HTTP requests.
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'), #'<int:pk>'  to capture the book id,
+    # which must be a specially formatted string, and pass it to the view as a parameter named pk (short for primary
+    # key). This is the id that is being used to store the book uniquely in the database, as defined in the Book Model.
+    path('authors', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -16,7 +16,7 @@ def index(request):
 
     # Count of genre and count of books containing a word
     # cw_books = Book.objects.filter(title__icontains = 'and').count()
-    # cw_genre = Book.objects.filter(genre__name__icontains = 'fIction').count() #Needed __name because genre is a ManyToMany mapping
+    # cw_genre = Book.objects.filter(genre__name__icontains = 'fIction').count() #Needed_name because genre is a ManyToMany mapping
 
     context = {
         'book_nr': book_nr,
@@ -29,3 +29,32 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    """That's it! The generic view will query the database to get all records for the specified model (Book) then render
+     a template located at /locallibrary/catalog/templates/catalog/book_list.html """
+    model = Book
+    paginate_by = 10
+    # Lots of stuff we can do:
+    # context_object_name = 'my_book_list'  # your own name for the list as a template variable
+    # queryset = Book.objects.filter(title__icontains='and')[:5]  # Get 5 books containing the title and
+    # template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+
+    # Add to context variables:
+    # def get_context_data(self, **kwargs):
+    #     # Call the base implementation first to get the context
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     # Create any data and add it to the context
+    #     context['more_data'] = 'Books are pretty cool!'
+    #     return context
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
